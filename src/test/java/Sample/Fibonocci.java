8e15a5c900cf1ba324ee50b19d020d9683c1195e
harish <harishhampapati@gmail.com>
2022-01-08 11:38:16 +0530

    Started

---
+++ src/test/java/Sample/Fibonocci.java
@@ -0,0 +1,31 @@
+package Sample;
+
+import java.util.Scanner;
+
+public class Fibonocci {
+   static Scanner sc;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sc= new Scanner(System.in);
+		int count = sc.nextInt();
+        fibonocci(count);
+		//System.out.println(Fibb);
+
+	}
+	private static void fibonocci(int count) {
+        
+		int first =0;
+		int second =1;
+		int sum=0;
+		System.out.print(first +" "+second);
+        for(int i=2;i<count;i++){
+        	System.out.print(" "+(sum=first+second)+" ");
+        	first=second;
+        	second=sum;
+        }
+		
+		 
+		
+	}
+
+}
